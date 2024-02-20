ffb0a1625d818b39200be199fb4900c8227ec0f1
constoner <constantin.kalinin@gmail.com>
2024-02-20 23:33:31 +0300

    connect badges with checkbox filters

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,9 @@
 import initSlider from './module/initSlider.js';
 import { initAccordions } from './vendor/accordion/init-accordion.js';
+import { initBadges } from './module/initBadges.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initSlider();
   initAccordions();
+  initBadges();
 });
---
+++ source/js/module/initBadges.js
@@ -0,0 +1,53 @@
+const badgeContainer = document.querySelector('#badge-container');
+const badgeTemplate = document.querySelector('#badge').content;
+const filterContainer = document.querySelector('#filter-container');
+const filterNodes = filterContainer.querySelectorAll('input[type="checkbox"]');
+
+const filters = Array.from(filterNodes).reduce((acc, item) => {
+  return Object.assign(acc, {
+    [item.value]: { title: item.dataset.title, node: item },
+  });
+}, {});
+
+const onBadgeClick = (filterArray, badge) => {
+  badgeContainer.removeChild(badge);
+  filterArray[badge.dataset.value].node.checked = false;
+};
+
+const createBadge = (node) => {
+  const badge = badgeTemplate.cloneNode(true).querySelector('*');
+  const badgeTitle = badge.querySelector('span');
+  const badgeButton = badge.querySelector('button');
+
+  badge.setAttribute('data-value', node.value);
+  badgeTitle.textContent = node.dataset.title;
+
+  badgeContainer.appendChild(badge);
+  badgeButton.addEventListener('click', () => onBadgeClick(filters, badge), { once: true });
+};
+
+const onFilterClick = (node) => {
+
+  if (node.checked !== true) {
+    const deletedBadge = badgeContainer.querySelector(`*[data-value="${node.value}"]`);
+    badgeContainer.removeChild(deletedBadge);
+  } else {
+    createBadge(node);
+  }
+
+};
+
+const initBadges = () => {
+
+  Object.values(filters).forEach((item) => {
+    item.node.addEventListener('change', () => onFilterClick(item.node));
+
+    if (item.node.hasAttribute('checked')) {
+      createBadge(item.node);
+    }
+
+  });
+
+};
+
+export { initBadges };
